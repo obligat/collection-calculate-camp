3681b1d3fad55ad8aa2c2301fed3b634e3a9b0b0
dujinqiao <duzi1996@qq.com>
2016-07-27 00:29:14 +0800

    number_map_to_word

--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -1,6 +1,6 @@
 'use strict';
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
-};
+  return collection.map((it)=>String.fromCharCode(96+it));
+}
 
 module.exports = number_map_to_word;
