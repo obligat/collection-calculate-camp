3356bedbb1e060e3ec85676a70cce9874997b0be
dujinqiao <duzi1996@qq.com>
2016-07-26 14:40:58 +0800

    get_integer_interval

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -1,8 +1,22 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-  let result=[];
-    result=
+  let result = [];
+  if (number_a < number_b) {
+    for (let i = number_a; i <= number_b; i++) {
+      result.push(i);
+    }
+  }
+  else if (number_a > number_b) {
+    for (let i = number_a; i >=number_b; i--) {
+      result.push(i);
+    }
+  }
+  else  {
+    result.push(number_a);
+  }
+
+  return result;
 }
 
 module.exports = get_integer_interval;
