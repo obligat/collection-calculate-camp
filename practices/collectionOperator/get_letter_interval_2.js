87d90604725896aa207900bbdf073677e5f5993e
dujinqiao <duzi1996@qq.com>
2016-07-26 16:22:40 +0800

    get_integer_2

--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -1,7 +1,64 @@
 'use strict';
 
 function get_letter_interval_2(number_a, number_b) {
-    //在这里写入代码
+
+    let result = [];
+    if (number_a < number_b) {
+        for (let i = number_a; i <= number_b; i++) {
+            if (i <= 26) {
+                let a = String.fromCharCode(64 + 32 + i);
+                result.push(a);
+            } else {
+                if (i % 26 === 0) {
+                    let a = String.fromCharCode(64 + 32 + i / 26 - 1);
+                    a += "z";
+                    result.push(a);
+                } else {
+                    let a = String.fromCharCode(64 + 32 + i / 26);
+                    a += String.fromCharCode(64 + 32 + i % 26);
+                    result.push(a);
+                }
+            }
+        }
+    }
+    else if (number_a > number_b) {
+        for (let i = number_a; i >= number_b; i--) {
+            if (i <= 26) {
+                let a = String.fromCharCode(64 + 32 + i);
+                result.push(a);
+            } else {
+                if (i % 26 === 0) {
+                    let a = String.fromCharCode(64 + 32 + i / 26 - 1);
+                    a += "z";
+                    result.push(a);
+                } else {
+                    let a = String.fromCharCode(64 + 32 + i / 26);
+                    a += String.fromCharCode(64 + 32 + i % 26);
+                    result.push(a);
+                }
+            }
+        }
+    }
+    else  {
+        if(number_a<=26){
+            let a = String.fromCharCode(64 + 32 + number_a);
+            result.push(a);
+        }
+        else {
+            if (number_a % 26 === 0) {
+                let a = String.fromCharCode(64 + 32 + number_a / 26 - 1);
+                a += "z";
+                result.push(a);
+            } else {
+                let a = String.fromCharCode(64 + 32 + number_a / 26);
+                a += String.fromCharCode(64 + 32 + number_a % 26);
+                result.push(a);
+            }
+
+        }
+    }
+    return result;
+
 }
 
 module.exports = get_letter_interval_2;
