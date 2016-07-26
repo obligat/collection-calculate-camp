0c1611dfea5d88e8f514f1ff49dfd31679a59cbb
dujinqiao <duzi1996@qq.com>
2016-07-26 22:49:25 +0800

    two_collections_practice

--- practices/filter/two_collections_practice_2.js
+++ practices/filter/two_collections_practice_2.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function choose_no_common_elements(collection_a, collection_b) {
+  let result = [];
+  result = collection_a.filter(function (it) {
+    return !collection_b.includes(it);
+  });
 
-  //在这里写入代码
+  return result;
 }
 
 module.exports = choose_no_common_elements;
