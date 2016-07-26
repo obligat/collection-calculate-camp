face919edb2bbd4562bce83ee941408bdf37d7f0
dujinqiao <duzi1996@qq.com>
2016-07-26 17:05:33 +0800

    no repeat

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function choose_no_repeat_number(collection) {
-
-  //在这里写入代码
+    let set=new Set(collection);
+    return Array.from(set);
 }
 
 module.exports = choose_no_repeat_number;
