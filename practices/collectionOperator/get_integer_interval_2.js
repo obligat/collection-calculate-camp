d24dca1db343bd9ad13d6f5ee85e3d42e156dd0f
dujinqiao <duzi1996@qq.com>
2016-07-26 12:14:14 +0800

    get_integer_interval

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,24 +1,34 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-    let result=[];
-    let arr=[number_a,number_b];
-   result=arr.reduce(function (a,b) {
-        let c=[];
-       if(a<b){
-          c.push(2);
-       }
-       else  if(a>b){
-           c=c-2;
-       }
-       else  if(a===b&&a/2===0){
-           c=a;
-       }
-       else {
+    let result = [];
 
-       }
-       return c;
-   });
+    function compare(a, b) {
+        let arr = [a, b];
+        if (arr[1] < arr[0]) {
+            arr[0] = b;
+            arr[1] = a;
+        }
+        return arr;
+    }
+
+    let arr = compare(number_a, number_b);
+    let a=0;
+    let length = Math.ceil((arr[1] - arr[0]) / 2);
+    if (number_a <number_b) {
+        for (let i = 0; i < length; i++) {
+            result.push(a = a + 2);
+        }
+    }
+       else  if(number_a>number_b){
+        for (let i = 0; i < length; i++) {
+            result.push(a = a + 2);
+        }
+        result=result.reverse();
+    }
+    else if (number_a === number_b&& number_a %2 === 0) {
+        result.push(number_a);
+    }
     return result;
 }
 
