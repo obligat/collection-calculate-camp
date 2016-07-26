c0078797e9e0cfd137938bf2bf86fb737ff2f811
dujinqiao <duzi1996@qq.com>
2016-07-26 16:47:56 +0800

    choose_multiples_of_three

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -2,7 +2,9 @@
 
 function choose_multiples_of_three(collection) {
 
-  //在这里写入代码
+  return collection.filter(function (item) {
+      return item%3===0;
+  });
 }
 
 module.exports = choose_multiples_of_three;
