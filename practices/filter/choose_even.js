1150dae082736792c0d7b19d68bac4fb01913e8b
dujinqiao <duzi1996@qq.com>
2016-07-26 16:44:42 +0800

    choose_even

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,8 +1,9 @@
 'use strict';
-                
+
 function choose_even(collection) {
-
-  //在这里写入代码
+    return collection.filter(function (num) {
+            return num % 2 === 0;
+        }
+    );
 }
-
 module.exports = choose_even;
