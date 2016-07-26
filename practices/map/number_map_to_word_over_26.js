911e18c1aa426ec23d14b50d34296b7b9e8ca8a9
dujinqiao <duzi1996@qq.com>
2016-07-27 00:21:12 +0800

    number_map_to_word_over_26

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,6 +1,19 @@
 'use strict';
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+  return collection.map(function (it) {
+    if(it<=26){
+      it=String.fromCharCode(96+it);
+    }
+    else if(it%26===0){
+     let a=String.fromCharCode(96+it/26-1);
+      it=a.concat('z');
+    }
+    else if(it%26!==0){
+      let a=String.fromCharCode(96+it/26);
+      it=a.concat(String.fromCharCode(96+it%26));
+    }
+    return it;
+  });
 };
 
 module.exports = number_map_to_word_over_26;
