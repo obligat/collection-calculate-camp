d674072b79cfd1cb5b8ca48552a4acdb9458fe91
dujinqiao <duzi1996@qq.com>
2016-07-26 23:19:33 +0800

    two_collection3

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -1,8 +1,16 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
+  let result=[];
 
-  //在这里写入代码
+    collection_a.filter(function (item) {
+      collection_b.filter(function (b) {
+        if(item%b===0){
+          result.push(item);
+        }
+      });
+    });
+ return result;
 }
 
 module.exports = choose_divisible_integer;
