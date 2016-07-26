e18fbf07e16c88ef468deb50382e8a8817b3d9da
dujinqiao <duzi1996@qq.com>
2016-07-26 14:56:35 +0800

    get_letter_interval

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -1,7 +1,21 @@
-'use strict';
+'use strict'
 
 function get_letter_interval(number_a, number_b) {
-  //在这里写入代码
+  let result=[];
+  if(number_a<number_b){
+    for(let i=number_a;i<=number_b;i++){
+      result.push(String.fromCharCode(64+32+i));
+    }
+  }
+  else if(number_a>number_b){
+    for(let i=number_a;i>=number_b;i--){
+      result.push(String.fromCharCode(64+32+i));
+    }
+  }
+  else {
+    result.push(String.fromCharCode(64+32+number_a));
+  }
+  return result;
 }
 
 module.exports = get_letter_interval;
