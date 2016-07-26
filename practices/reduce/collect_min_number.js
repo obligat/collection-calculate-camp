ed18f0d1746d4672b45d4b84e311eda72633fd66
dujinqiao <duzi1996@qq.com>
2016-07-27 00:52:37 +0800

    collect_min_number

--- practices/reduce/collect_min_number.js
+++ practices/reduce/collect_min_number.js
@@ -1,7 +1,13 @@
 'use strict';
 
 function collect_min_number(collection) {
-  //在这里写入代码
+  return collection.reduce(function (a, b) {
+    let min = a;
+    if (a > b) {
+      min = b;
+    }
+    return min;
+  });
 }
 
 module.exports = collect_min_number;
