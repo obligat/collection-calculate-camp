1e7284c3bb56b3d2cb8b5a30076aded918481212
dujinqiao <duzi1996@qq.com>
2016-07-27 10:33:31 +0800

    split to zero

--- practices/ruduce/split_to_zero.js
+++ practices/ruduce/split_to_zero.js
@@ -1,7 +1,12 @@
 'use strict';
 
 function spilt_to_zero(number, interval) {
-  //在这里写入代码
+  let result = [number];
+  for(let i=0;i<number/interval;i=number){
+    number=parseFloat((number-interval).toFixed(1));
+    result.push(number);
+  }
+  return result;
 }
 
 module.exports = spilt_to_zero;
