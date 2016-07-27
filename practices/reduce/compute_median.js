72be57662adc5ec909e58e3e32594239699af2af
dujinqiao <duzi1996@qq.com>
2016-07-27 08:57:31 +0800

    compute median

--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -1,7 +1,19 @@
 'use strict';
 
 function compute_median(collection) {
-  //在这里写入代码
+  function asc(a,b) {
+    return a-b;
+  }
+  collection=collection.sort(asc);
+  let leng = collection.length;
+  let middle = 0;
+  if (leng % 2 !== 0) {
+    middle = parseFloat(collection[(leng -1)/ 2]);
+  }
+  else {
+    middle = (parseFloat(collection[leng / 2 - 1]) + parseFloat(collection[leng / 2])) / 2;
+  }
+  return middle;
 }
 
 module.exports = compute_median;
